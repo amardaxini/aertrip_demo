66dd25c6f3288a5a4ca09e2407925d9fe25c5a00
Amar Daxini <amardaxini@gmail.com>
2017-10-27 04:45:42 +0530

    added sidekiq initializers

---
+++ config/initializers/sidekiq.rb
@@ -0,0 +1,21 @@
+require 'sidekiq'
+require 'sidekiq/web'
+
+
+
+if Rails.env == 'production' 
+
+  #namespace = ENV['REDIS_NAMESPACE'] || AppConfig.redis['namespace']
+  redis_url =ENV['REDISTOGO_URL']
+
+  Sidekiq.configure_client do |config|
+    config.redis = { :url => redis_url }
+    
+  end
+
+  Sidekiq.configure_server do |config|
+    config.redis = { :url => redis_url}
+    
+  end
+
+end
